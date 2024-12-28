4f10063f360d9cfc7cbd5958e8a1127a52db5550
CHAN YH <chan_yh@icloud.com>
2024-12-28 23:16:18 +0800

    Updated details

--- src/data/research.js
+++ src/data/research.js
@@ -1,15 +1,26 @@
-// TODO Add a couple lines about each project
+// Research Experience
 const data = [
   {
-    title: 'Blah',
-    subtitle: 'Blah',
-    image: '/images/projects/Blah.jpg',
-    date: '2024-05-29',
+    title: 'Machine Learning Techniques for Stock Price Prediction',
+    subtitle: 'w/ Dr. Joey Zhou',
+    // image: '/images/projects/Blah.jpg',
+    date: '2025-01-01 - 2025-11-30',
     desc:
-      'Blah. '
-      + 'Blah, '
-      + 'Blah '
-      + 'Blah.',
+      'Developed and implemented a Bayesian framework for stock price prediction, incorporating probabilistic forecasting to model market uncertainty and enhance trading decisions.'
+      + 'Designed and tested alpha generation strategies, leveraging momentum-based and mean-reversion techniques informed by predictive distributions.'
+      + 'Conducted backtesting on historical stock data to evaluate performance against traditional benchmarks, such as market indices and standard financial models.'
+      + 'Performed comparative analysis of Bayesian methods versus traditional and machine learning models, demonstrating improvements in alpha generation and market uncertainty modeling.',
+  },
+  {
+    title: 'Data-Driven Framework for Enhancing Supply Chain Resilience',
+    subtitle: 'w/ Dr. Liu Ning',
+    // image: '/images/projects/Blah.jpg',
+    date: '2024-11-01 - 2025-05-01',
+    desc:
+      'Conducted literature review and familiarization with End-to-End (E2E) inventory management systems and data-driven frameworks.'
+      + 'Simulated synthetic data, analyzed distribution patterns, and integrated real-world data into a Deep Learning model.'
+      + 'Developed and tested an E2E Reinforcement Learning-based approach, implementing and training models to optimize supply chain resilience.'
+      + 'Performed hyperparameter optimization, sensitivity analysis, and refined model designs to enhance performance metrics.',
   },
 ];
 
